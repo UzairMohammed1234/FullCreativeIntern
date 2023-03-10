c5f95a4aaef9161d1d13e4cfd729a806251ca7a8
uzair <uzair.zubair@anywhere.co>
2023-03-10 19:26:31 +0530

    WrapperClasses

---
+++ Month1_week1/src/WrapperClasses/FloatClass.java
@@ -0,0 +1,9 @@
+package WrapperClasses;
+
+public class FloatClass {
+	public static void main(String args[])
+	{
+		Float obj=new Float(10.1);
+		System.out.println(obj);
+	}
+}
---
+++ Month1_week1/src/WrapperClasses/IntegerClass.java
@@ -0,0 +1,9 @@
+package WrapperClasses;
+
+public class IntegerClass {
+	public static void main(String args[])
+	{
+		Integer obj=new Integer(10);
+		System.out.println(obj);
+	}
+}
