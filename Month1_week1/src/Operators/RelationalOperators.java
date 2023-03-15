4df7a2c59bf5813d633f7143d6fd0e5973124288
uzair <uzair.zubair@anywhere.co>
2023-03-15 18:59:06 +0530

    Incrementdecrement concept

--- Month1_week1/src/Operators/RelationalOperators.java
+++ Month1_week1/src/Operators/RelationalOperators.java
@@ -1,5 +1,35 @@
 package Operators;
-
+import java.util.*;
 public class RelationalOperators {
-
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		
+		// == operator
+		
+		System.out.println(a==b);
+		
+		// != 
+		
+		System.out.println(a!=b);
+		
+		// >
+		
+		System.out.println(a>b);
+		
+		// < 
+		
+		System.out.println(a<b);
+		
+		// >= 
+		
+		System.out.println(a>=b);
+		
+		//<=
+		
+		System.out.println(a<=b);
+		
+	}
 }
